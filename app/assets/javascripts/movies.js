b9f24913424ba21ed405f3c8cba71df9c90d86a7
Patchx <robert.anderson.fl@gmail.com>
2016-02-13 18:50:04 -0500

    Revert back to original css without javascript

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// (function() {
+//   var cards = document.querySelectorAll(".card.effect__click");
+//   for( var i  = 0; i < cards.length; i++ ) {
+//     var card = cards[i];
+//     clickListener( card );
+//   }
+
+//   function clickListener(card) {
+//     card.addEventListener( "click", function() {
+//       var c = this.classList;
+//       alert("dgge")
+//       c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+//     });
+//   }
+// })();
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,7 @@
 #
 # Examples:
 #
-movies = Movie.create([{ title: 'Anchorman: The Legend of Ron Burgundy', front_text: "I have many leatherbound books", front_image: "", question: "What is the movie?", back_text: "Anchorman: The Legend of Ron Burgundy", back_image: "http://i.imgur.com/9c8bG9H.jpg", hint: "Rich Mahogany", likes: 0, dislikes: 0}])
+movies = Movie.create([{ title: 'Anchorman: The Legend of Ron Burgundy', front_text: "I have many leatherbound books", front_image: "", question: "What is the movie?", back_text: "Anchorman: The Legend of Ron Burgundy", back_image: "http://i.imgur.com/9c8bG9H.jpg", hint: "Rich Mahogany", likes: 0, dislikes: 0}, 
+
+	{ title: 'Billy Madison', front_text: "", front_image: "http://i.imgur.com/U79B7Vk.jpg?1", question: "What is the movie?", back_text: "Billy Madison", back_image: "http://i.imgur.com/exNKIz5.jpg", hint: "blank", likes: 0, dislikes: 0}])
 
