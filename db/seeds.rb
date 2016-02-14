7758b10b4f1c9d2d083c31609d1b82844740f234
jmils <jmils66@gmail.com>
2016-02-13 20:26:46 -0500

    Fix html for front images and front quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,13 @@
 #
 # Examples:
 #
-movies = Movie.create([{ title: 'Anchorman: The Legend of Ron Burgundy', front_text: "I have many leatherbound books", front_image: "", question: "What is the movie?", back_text: "Anchorman: The Legend of Ron Burgundy", back_image: "http://i.imgur.com/9c8bG9H.jpg", hint: "Rich Mahogany", likes: 0, dislikes: 0}, 
+movies = Movie.create([{ title: 'Anchorman: The Legend of Ron Burgundy', front_text: '"I have many leatherbound books"', front_image: "", question: "What is the movie?", back_text: "Anchorman: The Legend of Ron Burgundy", back_image: "http://i.imgur.com/9c8bG9H.jpg", hint: "rich mahogany", likes: 0, dislikes: 0},
 
-	{ title: 'Billy Madison', front_text: "", front_image: "http://i.imgur.com/U79B7Vk.jpg?1", question: "What is the movie?", back_text: "Billy Madison", back_image: "http://i.imgur.com/exNKIz5.jpg", hint: "blank", likes: 0, dislikes: 0}])
+	{ title: 'Billy Madison', front_text: "", front_image: "http://i.imgur.com/U79B7Vk.jpg?1", question: "What is the movie?", back_text: "Billy Madison", back_image: "http://i.imgur.com/exNKIz5.jpg", hint: "blank", likes: 0, dislikes: 0},
 
+	{ title: 'Wedding Crashers', front_text: "Dude died in a hangliding accident. What an idiot.", front_image: "", question: "What is the movie?", back_text: "Wedding  Crashers", back_image: "http://i.imgur.com/hkPfaAqb.jpg", hint: "meatloaf", likes: 0, dislikes: 0},
+
+		{ title: 'Horrible Bosses', front_text: "", front_image: "", question: "What is the movie?", back_text: "Wedding  Crashers", back_image: "http://i.imgur.com/hkPfaAqb.jpg", hint: "meatloaf", likes: 0, dislikes: 0},
+
+
+	])
