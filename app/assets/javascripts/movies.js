054f3716a1411db342f8d7f275e7d844ee0e7d70
Stevie <steviepinero@gmail.com>
2016-02-14 15:08:35 -0500

    new flip method

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -15,23 +15,11 @@ $(document).ready(function(){
 });
 
     // new click implementation
-$(document).ready(function(){
-(function() {
-  console.log("New flip");
-  var cards = document.querySelectorAll(".card.effect__click");
-  for ( var i  = 0, len = cards.length; i < len; i++ ) {
-    var card = cards[i];
-    clickListener( card );
-  }
-
-  function clickListener(card) {
-    card.addEventListener( "click", function() {
-      var c = this.classList;
-      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    $(document).ready(function(){
+    $('.card').click(function(){
+      $(this).toggleClass('flipped');
     });
-  }
-});
-});
+  });
 
 // function () {
 //   var cards = document.querySelectorAll(".card.effect__click");
