4ecd16e97e4554acd0fc0e07e59fffa68444b492
jmils <jmils66@gmail.com>
2016-02-13 21:09:26 -0500

    Add movies to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,20 @@
 #
 # Examples:
 #
-movies = Movie.create([{ title: 'Anchorman: The Legend of Ron Burgundy', front_text: '"I have many leatherbound books"', front_image: "", question: "What is the movie?", back_text: "Anchorman: The Legend of Ron Burgundy", back_image: "http://i.imgur.com/9c8bG9H.jpg", hint: "rich mahogany", likes: 0, dislikes: 0},
+movies = Movie.create([{ title: 'Anchorman: The Legend of Ron Burgundy', front_text: "I have many leatherbound books", front_image: "", question: "What is the movie?", back_text: "Anchorman: The Legend of Ron Burgundy", back_image: "http://i.imgur.com/9c8bG9H.jpg", hint: "rich mahogany", likes: 0, dislikes: 0},
 
-	{ title: 'Billy Madison', front_text: "", front_image: "http://i.imgur.com/U79B7Vk.jpg?1", question: "What is the movie?", back_text: "Billy Madison", back_image: "http://i.imgur.com/exNKIz5.jpg", hint: "blank", likes: 0, dislikes: 0},
+	{ title: 'Billy Madison', front_text: "", front_image: "http://i.imgur.com/U79B7Vk.jpg?1", question: "What is the movie?", back_text: "Billy Madison", back_image: "http://i.imgur.com/exNKIz5.jpg", hint: "back to school", likes: 0, dislikes: 0},
 
-	{ title: 'Wedding Crashers', front_text: "Dude died in a hangliding accident. What an idiot.", front_image: "", question: "What is the movie?", back_text: "Wedding  Crashers", back_image: "http://i.imgur.com/hkPfaAqb.jpg", hint: "meatloaf", likes: 0, dislikes: 0},
+	{ title: 'Wedding Crashers', front_text: "Dude died in a hang gliding accident. What an idiot.", front_image: "", question: "What is the movie?", back_text: "Wedding  Crashers", back_image: "http://i.imgur.com/hkPfaAqb.jpg", hint: "meatloaf", likes: 0, dislikes: 0},
 
-		{ title: 'Horrible Bosses', front_text: "", front_image: "", question: "What is the movie?", back_text: "Wedding  Crashers", back_image: "http://i.imgur.com/hkPfaAqb.jpg", hint: "meatloaf", likes: 0, dislikes: 0},
+	{ title: 'Horrible Bosses', front_text: "", front_image: "http://i.imgur.com/9T26lBH.jpg?1", question: "What is the movie?", back_text: "Horrible Bosses", back_image: "http://i.imgur.com/OX09J2S.jpg", hint: "dentist", likes: 0, dislikes: 0},
 
+	{ title: 'Tommy Boy', front_text: "Does this suit make me look fat?", front_image: "", question: "What is the movie?", back_text: "Tommy Boy", back_image: "http://i.imgur.com/Cv6YFuK.jpg", hint: "Fat guy in a little coat", likes: 0, dislikes: 0},
+
+	{ title: 'Zoolander', front_text: "TODD! Where's my goddamn latté?", front_image: "", question: "What is the movie?", back_text: "Tommy Boy", back_image: "http://i.imgur.com/bVgTPmB.jpg", hint: "Relax, don't do it.", likes: 0, dislikes: 0},
+
+	{ title: 'Wedding Crashers', front_text: "Don't ever leave me...cause I'd find you.", front_image: "", question: "What is the movie?", back_text: "Wedding  Crashers", back_image: "http://i.imgur.com/oRGnzYZ.jpg", hint: "meatloaf", likes: 0, dislikes: 0},
+
+	{ title: 'Tommy Boy', front_text: "", front_image: "http://i.imgur.com/J84C5ju.jpg?1", question: "What is the movie?", back_text: "Tommy Boy", back_image: "http://i.imgur.com/aM4RKa6.jpg", hint: "Callahan auto parts", likes: 0, dislikes: 0},
 
 	])
